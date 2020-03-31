eb4e76e27eab2eaf1a56cded92680af37daaea3a
Laurence Liang <laurencel2001@gmail.com>
2020-03-31 13:49:55 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,36 @@
 import random
 
+cat = ""
 joke_list = ["I ate a clock yesterday, it was very time-consuming.",
 "Did you hear about the crook who stole a calendar? He got twelve months.",
 "I own the world’s worst thesaurus. Not only is it awful, it’s awful.",
 "I’ve just written a song about tortillas; actually, it’s more of a rap."]
 
-while True:
-  user_response = input("User input: ")
+class ChatBot:
+    def __init__(self):
+        self.user_name = ''
 
-  if "goodbye" in user_response.lower().split(' '):
-    print("Goodbye fellow human!")
-    break
+    def set_user_name(self, user_name):
+        self.user_name = user_name
 
-  elif user_response.lower() == "how are you":
-    print("I am doing good")
 
-  elif user_response.lower() ==  "tell me a joke":
-    print(random.choice(joke_list))
-    
+jeff = ChatBot()
+
+while not cat == "goodbye":
+  cat = input("Hello " +jeff.user_name+": ").lower() # user input
+
+
+  if cat == "hello":
+    print("hello back")
+  elif "my name is" in cat:
+    print(cat)
+    print(cat.split("my name is"))
+    user_name = cat.split("my name is")[1].strip()
+    jeff.set_user_name(user_name)
+  elif cat == "how are you?":
+    print("I'm doing well.")
+  elif cat == "goodbye":
+    print("Bye fellow human!")
   else:
-    print("Why is " + user_response +"?")
+    print(random.choice(joke_list))
+
